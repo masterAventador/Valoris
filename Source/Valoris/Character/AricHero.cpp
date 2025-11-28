df45ccee6d8fe1a1398aff97ad14c35fef74ae14
Aventador <419460185@qq.com>
2025-11-29 00:55:42 +0800

    添加英雄 Aric，更新英雄设计文档

---
+++ Source/Valoris/Character/AricHero.cpp
@@ -0,0 +1,8 @@
+// Copyright Valoris. All Rights Reserved.
+
+#include "AricHero.h"
+
+AAricHero::AAricHero()
+{
+	// Aric 是近战战士，可以在蓝图中配置具体属性
+}
---
+++ Source/Valoris/Character/AricHero.h
@@ -0,0 +1,20 @@
+// Copyright Valoris. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ValorisCharacterBase.h"
+#include "AricHero.generated.h"
+
+/**
+ * 英雄：Aric（战士）
+ * 近战物理输出，高生命值，技能带冲锋和AOE
+ */
+UCLASS()
+class VALORIS_API AAricHero : public AValorisCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AAricHero();
+};
